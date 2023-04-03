76a7f1fe9a121343ea5a0d28c89f0d562b058e45
AgenTwilight <mhmmad.iqbal75@gmail.com>
2023-04-03 03:22:24 -0400

    Kumpul Praktikum 3

---
+++ src/H071221087/Pertemuan_3/No1.java
@@ -0,0 +1,83 @@
+package Praktikum3;
+// Novel Bumi
+class NovelBumi{
+    String authorName;
+    String genre;
+    int rating;
+    Karakter[] karakter;
+
+    public NovelBumi(String authorName, String genre, int rating, int jumlahKarakter){
+        this.authorName = authorName;
+        this.genre = genre;
+        this.rating = rating;
+        this.karakter = new Karakter[jumlahKarakter];
+    }
+
+    public NovelBumi (){
+        this.karakter = new Karakter[2];
+    }
+
+    public void addKarakter (Karakter karakter, int index){
+        this.karakter[index-1] = karakter;
+    }
+
+    public void setAuthorName(String author){
+        authorName = author;
+    }
+    public void setGenre(String genreString){
+        genre = genreString;
+    }    
+    public void setRating(int rating){
+        this.rating = rating;
+    }
+    public void displayDetail() {
+        System.out.println("Novel Bumi");
+        System.out.println("==========");
+        System.out.printf("Author : %s\nGenre : %s\nRating : %s\n" , authorName, genre, rating);
+        for (int i = 0; i < karakter.length; i++) {
+            System.out.println("\n");
+            System.out.printf("Karakter %d\n", i+1);
+            System.out.println("==========");
+            System.out.println("Nama : "+ karakter[i].nama);
+            System.out.println("Klan : "+ karakter[i].klan);
+            System.out.println("Skill : "+ karakter[i].skill);
+        }
+}
+}
+
+class Karakter{
+    String nama;
+    String klan;
+    String skill;
+
+    public Karakter(String nama, String klan, String skill){
+        this.nama = nama;
+        this.klan = klan;
+        this.skill = skill;
+    }
+
+    public void setName(String nama){
+        this.nama = nama; 
+    }
+    public void setKlan(String klan ){
+        this.klan = klan;
+    }
+    public void setSkill(String skill){
+        this.skill = skill;
+    }
+}
+
+
+public class No1 {
+    public static void main(String[] args) {
+        Karakter karakter1 = new Karakter("Raib", "Bulan", "Menghilang");
+        Karakter karakter2 = new Karakter("Seli", "Matahari", "Telekinesis");
+        NovelBumi novelBumi = new NovelBumi();
+        novelBumi.setAuthorName("TereLiye");
+        novelBumi.setGenre("Fantasi");
+        novelBumi.setRating(5);
+        novelBumi.addKarakter(karakter1,1);
+        novelBumi.addKarakter(karakter2,2);
+        novelBumi.displayDetail();
+    }
+}
---
+++ src/H071221087/Pertemuan_3/No2.java
@@ -0,0 +1,49 @@
+package Praktikum3;
+
+class Player{
+        private String nama;
+        private int hp;
+        private int attackPower;
+        private int defense;
+        public Player(){}
+        
+        public void getDamage(Player enemy) {
+            hp= hp-Math.abs(enemy.getAttackPower()-defense);
+            }
+
+        // Lengkapi
+        public void setAttackPower(int attackPower){
+            this.attackPower = attackPower;
+            }       
+        public Player(String nama,int attackPower,int defense){
+            this.hp = 100;
+            this.nama = nama;
+            this.attackPower = attackPower;
+            this.defense = defense; 
+            }
+        public Player(String nama,int defense){
+            this.hp = 100;
+            this.nama = nama;
+            this.defense = defense; 
+            }
+
+        public void status() {
+            System.out.println(nama + " Status");
+            System.out.println("HP = " + hp);
+            System.out.println("Defense = " + defense);
+            System.out.println("Attack = " + attackPower + "\n");
+            }
+        int getAttackPower() {
+            return attackPower;
+            }
+    }
+    public class No2 {
+    public static void main(String [] args) {
+        Player player1= new Player("Mino", 30, 15);
+        Player player2= new Player("Hilda", 10);
+        player2.setAttackPower(35);
+        player1.getDamage(player2);
+        player1.status();
+        player2.status();
+        }
+    }
