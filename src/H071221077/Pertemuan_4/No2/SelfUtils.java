b34d225d255eb2c8b6cf9c7e5228731e765ca047
fadillahcheryl <fadillahcheryl88@gmail.com>
2023-04-12 14:56:39 +0800

    Tugas Praktikum 4

---
+++ src/H071221077/Pertemuan_4/No2/SelfUtils.java
@@ -0,0 +1,8 @@
+package No2;
+
+class SelfUtils {
+    public static void displaySelfData(){
+        System.out.println("Nama\t: Fadillah Cheryl");
+        System.out.println("NIM\t: H071221077");
+    }
+}
