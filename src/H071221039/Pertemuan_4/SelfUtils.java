bcf4fb959829d75cc178b5d95d5cc22d5632c186
MuhKhaekal <muh.khaekal@gmail.com>
2023-04-13 15:03:38 +0700

    Kumpul Tugas 4

---
+++ src/H071221039/Pertemuan_4/SelfUtils.java
@@ -0,0 +1,7 @@
+package H071221039.Pertemuan_3;
+public class SelfUtils {
+    public static void displaySelfData(){
+        System.out.println("Nama : Khaekal");
+        System.out.println("NIM : H071221039");
+    }
+}
