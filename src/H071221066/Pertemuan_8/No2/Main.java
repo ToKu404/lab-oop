3ea368dfabccb01fc9350d17f0c25f82929d41c1
ZabrynaAndiny11 <zabryna.andiny@gmail.com>
2023-05-16 15:33:28 +0800

    Tugas Praktikum 8

---
+++ src/H071221066/Pertemuan_8/No2/Main.java
@@ -0,0 +1,22 @@
+package No2;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        TypeRacer typeRacer = new TypeRacer();
+        typeRacer.setNewWordsToType();
+        System.out.println("||  Text to Type  ||");
+        System.out.println("\"" + typeRacer.getWordsToType() + "\"");
+
+        Typer[] typers = new Typer[3];
+
+        typers[0] = new Typer("Bot EXO", 80, typeRacer);
+        typers[1] = new Typer("Bot NCT", 80, typeRacer);
+        typers[2] = new Typer("Bot 17", 40, typeRacer);
+
+        typeRacer.getRaceContestant().addAll(Arrays.asList(typers));
+
+        typeRacer.startRace();
+    }
+}
